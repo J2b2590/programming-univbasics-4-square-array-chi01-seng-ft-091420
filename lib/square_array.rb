ec2639c199735113001ff97d9bfc2704ac0aeebd
Jeffrey Bolling <j2b2590@gmail.com>
2020-08-25 23:01:38 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,19 @@
+
+
+
+
+
+
+
 def square_array(array)
-  # your code here
+counter = 0
+new_nums_array = []
+
+while counter < array.length do
+  new_nums_array.push(array[counter] * array[counter])
+  counter += 1
+  end
+new_nums_array
 end
+
+puts square_array([12,3,233,32123,43])
